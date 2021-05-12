99af163796fe2f4538386353b7189324a75f3106
Abhishek Kumar <abhishek022kk@gmail.com>
2021-05-12 12:30:30 +0530

    added the links to projects, contact, skills and experience

--- js/main.js
+++ js/main.js
@@ -1,7 +1,14 @@
 const about = document.querySelector("#about");
 const contact = document.querySelector("#contact");
+const skills = document.querySelector("#skills");
+const projects = document.querySelector("#projects");
+const exprience = document.querySelector("#exprience");
+
 const aboutContent = document.querySelector("#about-content");
 const contactContent = document.querySelector("#contact-content");
+const skillsContent = document.querySelector("#skills-content");
+const projectsContent = document.querySelector("#projects-content");
+const experienceContent = document.querySelector("#experience-content");
 
 about.addEventListener("click", () => {
   const aboutBox = new WinBox({
@@ -34,6 +41,67 @@ contact.addEventListener("click", () => {
     bottom: 50,
     left: 250,
     mount: contactContent,
+    onfocus: function () {
+      this.setBackground("#00aa00");
+    },
+    onblur: function () {
+      this.setBackground("#777");
+    },
+  });
+});
+
+skills.addEventListener("click", () => {
+  const skillsBox = new WinBox({
+    title: "Skills",
+    background: "#00aa00",
+    width: "400px",
+    height: "400px",
+    top: 150,
+    right: 50,
+    bottom: 50,
+    left: 250,
+    mount: skillsContent,
+    onfocus: function () {
+      this.setBackground("#00aa00");
+    },
+    onblur: function () {
+      this.setBackground("#777");
+    },
+  });
+});
+
+projects.addEventListener("click", () => {
+  const projectsBox = new WinBox({
+    title: "Projects",
+    background: "#00aa00",
+    width: "400px",
+    height: "400px",
+    top: 150,
+    right: 50,
+    bottom: 50,
+    left: 250,
+    mount: projectsContent,
+    onfocus: function () {
+      this.setBackground("#00aa00");
+    },
+    onblur: function () {
+      this.setBackground("#777");
+    },
+  });
+});
+
+
+experience.addEventListener("click", () => {
+  const experienceBox = new WinBox({
+    title: "experience",
+    background: "#00aa00",
+    width: "400px",
+    height: "400px",
+    top: 150,
+    right: 50,
+    bottom: 50,
+    left: 250,
+    mount: experienceContent,
     onfocus: function () {
       this.setBackground("#00aa00");
     },
